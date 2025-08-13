4e2d2c75bb6dd93394acc74ab9e8be768755bb0f
Kai'den Engle <kaidenengle8@gmail.com>
2025-08-13 18:44:49 -0400

    Tried and failed ;[]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,11 @@
-"use client";
-
-import Image from "next/image";
-import { useEffect, useRef } from "react";
-import { createChart, LineSeries } from "lightweight-charts";
-
-const chart = createChart(document.body, { width: 400, height: 300 });
-const lineSeries = chart.addSeries(LineSeries);
-lineSeries.setData([
-  { time: "2019-04-11", value: 80.01 },
-  { time: "2019-04-12", value: 96.63 },
-  { time: "2019-04-13", value: 76.64 },
-  { time: "2019-04-14", value: 81.89 },
-  { time: "2019-04-15", value: 74.43 },
-  { time: "2019-04-16", value: 80.01 },
-  { time: "2019-04-17", value: 96.63 },
-  { time: "2019-04-18", value: 76.64 },
-  { time: "2019-04-19", value: 81.89 },
-  { time: "2019-04-20", value: 74.43 },
-]);
+import Test from "@/components/test";
+import Page from "@/components/loader";
 
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <h1>Hello world!</h1>
+      <Test />
     </div>
   );
 }
